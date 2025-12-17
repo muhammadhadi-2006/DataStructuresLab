c31647dbce17e2e359e7d247ce9c7f4846c81cae
Muhammad Hadi <muhammadhadi20060@gmail.com>
2025-12-17 22:24:27 +0500

    Create main.cpp

---
+++ Sorting _Algorithms/Selection_Sort/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+#include <fstream>
+#include<sstream>
+#include <string>
+#include <chrono>
+using namespace std::chrono;
+void SelectionSort(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        int smallestIndex=i;
+        for (int j=i+1;j<n;j++){
+            if (arr[i]==arr[j]){
+                continue;
+
+            }
+            if (arr[j]<arr[smallestIndex]){
+                smallestIndex=j;
+            }
+        }
+    swap(arr[i],arr[smallestIndex]);
+    }
+}
+
+int main() {
+    ifstream fin("numbers.txt");
+    if (!fin){
+        cout<<"error in opening ";
+        return 0;
+    }
+
+    string line, token;
+    getline(fin, line);
+    fin.close();
+    int count=1;
+    for (char c:line){
+        if (c==','){
+            count++;
+        }
+    }
+
+    int * arr=new int [count];
+    int n=0;
+    stringstream ss(line);
+    while (getline(ss,token,',')){
+        arr[n++]=stoi(token);
+    }
+
+ 
+     auto start=high_resolution_clock::now();
+    SelectionSort(arr, n);
+    auto stop=high_resolution_clock::now();
+    auto duration =duration_cast<milliseconds>(stop - start);
+  
+
+    
+
+    cout << "Sorted Array: ";
+    for(int i = 0; i < n; i++)
+        {cout << arr[i] << " ";}
+    cout<<"time taken by bubble sort:"<<duration.count()<<"milli second";
+      
+
+    return 0;
+}
